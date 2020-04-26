506efe97bb669f541c09a4b84b12293bd337c6ad
Alexander Pester <alexander.pester@softwareag.com>
2020-04-26 22:00:22 +0200

    remove REST controller from App class

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -2,12 +2,8 @@ package ${package};
 
 import com.cumulocity.microservice.autoconfigure.MicroserviceApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @MicroserviceApplication
-@RestController
 public class App {
     public static void main (String[] args) {
         SpringApplication.run(App.class, args);
