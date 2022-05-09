874b21833d404fbd3ec2ee3ec90485d74810b31f
Pester <apes@softwareag.com>
2022-05-09 09:56:12 +0200

    groovy script finished

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,9 +1,15 @@
 import groovy.json.JsonSlurper
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.io.File 
 
 println("########## C8y Dev Tools, Local microservice runtime setup ################")
 println("Cumulocity URL: $devC8yBaseURL")
 println("User Credentials: $devC8yUserCredentialsBASE64")
 println("Microservice name: $microserviceName")
+
+def jsonSlurper = new JsonSlurper()
 
 // Step 1.1: Create application on Cumulocity Tenant
 def post1 = new URL("$devC8yBaseURL/application/applications").openConnection();
@@ -41,7 +47,7 @@ println(postRC1);
 
 def responseBody1 = post1.getInputStream().getText()
 println(responseBody1);
-def jsonSlurper = new JsonSlurper()
+
 def object1 = jsonSlurper.parseText(responseBody1)
 
 def msInternalId = object1.id
@@ -70,4 +76,32 @@ get3.setRequestProperty("Content-Type", "application/json")
 get3.setRequestProperty("Authorization", devC8yUserCredentialsBASE64)
 def getRC3 = get3.getResponseCode();
 println(getRC3);
-println(get3.getInputStream().getText());
+def responseBody3 = get3.getInputStream().getText()
+def object3 = jsonSlurper.parseText(responseBody3)
+
+def devC8yBootstrapUser = object3.name
+def devC8yBootstrapPassword = object3.password
+def devC8yTenantId = object3.tenant
+
+def propertyFileText = """
+#Do not edit, version properties are set at build time
+c8y.version=@c8y.version@
+microservice.version=@project.version@
+
+#Cumulocity configuration for running localy and connecting to cumulocity
+application.name=$microserviceName
+C8Y.bootstrap.register=true
+C8Y.bootstrap.tenant=$devC8yTenantId
+C8Y.baseURL=$devC8yBaseURL
+C8Y.bootstrap.user=$devC8yBootstrapUser
+C8Y.bootstrap.password=$devC8yBootstrapPassword
+C8Y.microservice.isolation=MULTI_TENANT
+"""
+println(propertyFileText)
+Path projectPath = Paths.get(request.outputDirectory, request.artifactId)
+println(projectPath)
+def filePath = projectPath.toString() + "\\src\\main\\resources\\application-dev.properties"
+println("Write properties file: $filePath")
+File file = new File(filePath)
+file.write(propertyFileText)
+println("########## C8y Dev Tools, Local microservice runtime setup ################")
